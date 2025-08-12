2640a71da731a2e3c27a07042b2ecb84a6354d60
Nattalic <nordonez376@gmail.com>
2025-08-12 10:56:54 -0500

    iniciamos js

---
+++ Practica-JS-DCA/index.js
@@ -0,0 +1,30 @@
+
+// formas de crear variables
+var miVariable = 1 
+//variables globales
+
+const esUnaConstante = true //las que tenemos que usar
+//solo se pueden usar una vez
+
+let variableY= "texto largo" //las que tenemos que usar
+//varias veces las puedes utilizar
+
+
+//si no nombramos o le asignamos un nombre a la variable , el codigo tomara eso como un var, que es lo que no queremos
+x = 1 //asignacion, se asigna el valor de 1 a x
+console.log(x) 
+
+
+var y = 
+console.log(y) //undifined 
+
+//hosting: antes de que se ejecute el codigo, un programa de javascript 
+// recorre el codigo y crea variables que se vayan a utilizar
+
+const variableA = "esto es un string" + "una nueva palabra"//cadenas de texto , se pueden juntar
+const variableB = true 
+const variableC = 1 //numeros
+
+console.log(typeof variableA) //string
+console.log(typeof variableB) //boolean
+console.log(typeof variableC) //number
